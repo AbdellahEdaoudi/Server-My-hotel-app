fcedf0d745b9a648d508103168b670a46bf7c968
abdellah <abdellahedaoudi80@gmail.com>
2024-05-30 01:15:39 +0100

    ADMIN

--- index.js
+++ index.js
@@ -160,7 +160,98 @@ app.post("/login", async (req, res) => {
     name: foundUser.name,
   });
 });
+app.post('/Adminregister', async (req, res) => {
+  const { name, email, pass } = req.body;
+  if (!name || !email || !pass) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const foundUser = await User.findOne({ email }).exec();
+  if (foundUser) {
+    return res.status(401).json({ message: "User already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(pass, 10);
 
+  const user = await new User({
+    name,
+    email,
+    pass: hashedPassword,
+  });
+  await user.save();
+  const accessTokenAdmin = jwt.sign(
+    {
+      UserInfo: {
+        id: user._id,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "30m" }
+  );
+  const refreshToken = jwt.sign(
+    {
+      UserInfo: {
+        id: user._id,
+      },
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true, //accessible only by web server
+    secure: true, //https
+    sameSite: "None", //cross-site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+  res.json({
+    accessToken: accessTokenAdmin,
+    email: user.email,
+    name: user.name,
+  });
+})
+
+// login 
+app.post("/Adminlogin", async (req, res) => {
+  const { email, pass } = req.body;
+  if (!email || !pass) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const foundUser = await User.findOne({ email }).exec();
+  if (!foundUser) {
+    return res.status(401).json({ message: "User does not exist" });
+  }
+  const match = await bcrypt.compare(pass, foundUser.pass);
+
+  if (!match) return res.status(401).json({ message: "Wrong Password" });
+
+  const accessTokenAdmin = jwt.sign(
+    {
+      UserInfo: {
+        id: foundUser._id,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "60m" }
+  );
+  const refreshToken = jwt.sign(
+    {
+      UserInfo: {
+        id: foundUser._id,
+      },
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true, //accessible only by web server
+    secure: true, //https
+    sameSite: "None", //cross-site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+  res.json({
+    accessToken: accessTokenAdmin,
+    email: foundUser.email,
+    name: foundUser.name,
+  });
+});
 // refresh
 app.get("/refresh", async (req, res) => {
   const cookies = req.cookies;
