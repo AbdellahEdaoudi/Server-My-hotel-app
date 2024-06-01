f5089a876bc432eaca4d4074e382177a93572011
abdellah <abdellahedaoudi80@gmail.com>
2024-06-01 19:32:39 +0100

    Admin Cors

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const nodemailer = require("nodemailer")
 app.use((req, res, next) => {
  // https://edhotel.vercel.app
   res.setHeader('Access-Control-Allow-Origin', 'https://edhotel.vercel.app');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
