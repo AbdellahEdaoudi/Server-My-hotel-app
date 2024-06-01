163404264d3d8271672ad2c1f8a643792f449423
abdellah <abdellahedaoudi80@gmail.com>
2024-06-01 03:20:34 +0100

    SendEmail

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ const upload = require("./middleware/multer");
 const nodemailer = require("nodemailer")
 
 // CORS middleware with specific origin
-app.use(cors({
-  // origin: 'https://edhotel.vercel.app',
-  origin: 'https://edhotel.vercel.app',
-  methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
-  credentials: true
-}));
+app.use((req, res, next) => {
+ // https://edhotel.vercel.app
+  res.setHeader('Access-Control-Allow-Origin', 'https://edhotel.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 // VerifyToken middleware
 const VerifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
