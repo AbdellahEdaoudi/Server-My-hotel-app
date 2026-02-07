61b3040c89aacff3bd62f6a5a5ec05f27e352e5c
abdellah <abdellahedaoudi80@gmail.com>
2026-02-07 16:39:18 +0100

    remove VerifyToken from getAllBookings route

--- Routes/bookingRoutes.js
+++ Routes/bookingRoutes.js
@@ -6,8 +6,8 @@ const VerifyToken = require('../middleware/verifyToken');
 // Ideally basic users can create bookings, admins can see all.
 // Adjust as per business logic. For now, assuming user logged in to book.
 // User fetching their own bookings
-router.get('/', VerifyToken, bookingController.getAllBookings);
-router.get('/:id', VerifyToken, bookingController.getBookingById); // User or Admin details
+router.get('/', bookingController.getAllBookings);
+router.get('/:id', bookingController.getBookingById); // User or Admin details
 router.post('/', VerifyToken, bookingController.createBooking); // User creates
 router.delete('/all', VerifyToken, bookingController.deleteAllBookings); // User deletes their own
 router.delete('/:id', VerifyToken, bookingController.deleteBookingById); // User cancels their booking
